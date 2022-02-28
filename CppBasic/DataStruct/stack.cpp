63578402bf4792b022d19f8cf48b6067fa490613
undooo <lhk2783497478@outlook.com>
2022-02-28 15:09:43 +0800

    master

--- CppBasic/DataStruct/stack.cpp
+++ CppBasic/DataStruct/stack.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 
 
+void StackFunction()
+{   stack<int> S;
+    //pop返回的值是void，而不是int
+    S.pop();
+    //top返回的是值
+    int t=S.top();
+}
--- 排序算法/StackSort.cpp
+++ 排序算法/StackSort.cpp
@@ -1,3 +1,18 @@
 #include"stdc++.h"
 using namespace std;
 
+//栈排序
+stack<int> StackSort(stack<int> &R)
+{
+    stack<int> S;
+    int t=R.top();
+    R.pop();
+    while(!R.empty()){
+        if(S.empty() || S.top()<=t){
+            S.push(t);
+            t=R.pop();
+        }
+    }
+    //最后还有一个t没有被放入
+    S.push(t);
+}
